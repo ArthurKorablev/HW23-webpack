fd93523d4b0715e619d082e5b81947bd96d13b8c
ArthurKorablev <arttr86@gmail.com>
2023-02-20 21:53:16 +0200

    js code

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/app.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+        {
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [
+              {
+                loader: 'file-loader',
+              },
+            ],
+          },
+          {
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            type: "asset",
+          },
+      ],
+  },
+  optimization: {
+    minimizer: [
+      "...",
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.squooshMinify,
+          options: {
+          },
+        },
+      }),
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin(), new MiniCssExtractPlugin()],
+};
